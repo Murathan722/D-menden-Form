c1190a14b387ea90ff3bbb8c7960eb711bd18098
Murathan Abanoz <126447496+Murathan722@users.noreply.github.com>
2024-06-27 20:18:25 +0300

    FormArea güncellendi

--- src/components/FormArea.jsx
+++ src/components/FormArea.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import "../css/formArea.css";
 
 const initialValues = {
   firstName: "",
@@ -12,46 +13,81 @@ const initialValues = {
   Cinsiyet: "",
 };
 
-const FormArea = () => {
+const FormArea = ({ formData, setFormData }) => {
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
+    reset,
   } = useForm({
     defaultValues: initialValues,
     mode: "onChange",
   });
 
-  const onSubmit = (data) => {
-    setFormData(data);
-    console.log("Form submitted: ", data);
+  const onSubmit = (event) => {
+    setFormData(event);
+    console.log("Form submitted: ", event);
+    reset();
+  };
+
+  const handleClear = () => {
+    setFormData(initialValues);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>First Name: </label>
-        <input
-          {...register("firstName", { required: "First name is required" })}
-        />
-        {errors.firstName && <p>{errors.firstName.message}</p>}
-      </div>
-      <div>
-        <label>Last Name: </label>
-        <input
-          {...register("lastName", { required: "Last name is required" })}
-        />
-        {errors.lastName && <p>{errors.lastName.message}</p>}
-      </div>
-      <div>
-        <label>Email: </label>
-        <input {...register("ePosta", { required: "Email is required" })} />
-        {errors.ePosta && <p>{errors.ePosta.message}</p>}
-      </div>
-      <button type="submit" disabled={!isValid}>
-        Submit
-      </button>
-    </form>
+    <div className="container">
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
+        <div className="input-area">
+          <label>
+            First Name:{" "}
+            <input
+              {...register("firstName", { required: "First name is required" })}
+            />
+            {errors.firstName && <p>{errors.firstName.message}</p>}
+          </label>
+        </div>
+        <div className="input-area">
+          <label>
+            Last Name:{" "}
+            <input
+              {...register("lastName", { required: "Last name is required" })}
+            />
+            {errors.lastName && <p>{errors.lastName.message}</p>}
+          </label>
+        </div>
+        <div className="input-area">
+          <label>
+            Email:{" "}
+            <input {...register("ePosta", { required: "Email is required" })} />
+            {errors.ePosta && <p>{errors.ePosta.message}</p>}
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              {...register("box", { required: "Box is required" })}
+            />
+            {errors.box && <p>{errors.box.message}</p>}
+            <input
+              type="checkbox"
+              {...register("box2", { required: "Box2 is required" })}
+            />
+            <input
+              type="checkbox"
+              {...register("box3", { required: "Box3 is required" })}
+            />
+            <input
+              type="checkbox"
+              {...register("box4", { required: "Box4 is required" })}
+            />
+          </label>
+        </div>
+        <button type="submit" disabled={!isValid}>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
